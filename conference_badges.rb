c3f03f59b911abee3c7f3bf157517035409f385f
Roberto Samia <bojosteph@gmail.com>
2018-09-17 00:50:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def conference_badges
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  badges0 = attendees(0)
+  badges0 = attendees[0]
   puts "Hello,my name is #{badges0}"
 end
