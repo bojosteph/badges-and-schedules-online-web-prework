a757b0a5c28d61427a371194a629844f223f3a98
Roberto Samia <bojosteph@gmail.com>
2018-09-17 01:01:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def conference_badges
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  attendees[0] = name1
-  puts "Hello,my name is  (#"name1")."
+  attendees = {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+  
+  puts "Hello,my name is {[]} "
 end
